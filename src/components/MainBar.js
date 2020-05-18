ffd4831310955e864fd2ffc6d231414a0215ca0c
akrukovich <anatoliinakrukovich@gmail.com>
2020-05-18 21:23:24 +0300

    Add first api methods

--- src/components/MainBar.js
+++ src/components/MainBar.js
@@ -7,50 +7,116 @@ class CheckBox extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      checked:false
+      toggle: false
     }
+    this.toggleState = this.toggleState.bind(this);
+
   }
+
+  toggleState() {
+    this.setState({
+      toggle: !this.state.toggle
+    });
+  }
+
 
   render() {
     return (
       <label className="joke_search__label">
-        <input className="joke_search__option" type="radio" checked={this.state.checked}  />
+        <input className="joke_search__option" value={this.props.value}
+               onChange={this.toggleState}
+               checked={this.state.toggle} type="radio"/>
         {this.props.option}
       </label>
     );
   }
 }
 
-class Categories extends Component{
+class Categories extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      categories : []
+      categories: []
     }
   }
+
   componentDidMount() {
     fetch('https://api.chucknorris.io/jokes/categories')
       .then((response) => {
         return response.json();
       })
       .then((data) => {
-        this.setState({categories:data})
+        this.setState({categories: data})
       });
   }
 
   render() {
-    const categories = this.state.categories
+    const categories = this.state.categories.map(category => <Category name={category}/>)
     return (
-      categories.map(category=> <Category name={category}/>)
+      <div className="joke-search__categories">
+        {categories}
+      </div>
     )
 
   }
 }
+
 function Category({name}) {
-  return <div>{name}</div>
+  return <span className='joke-search__category'>{name}</span>
+}
+
+class Jokes extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    const jokes = this.props.jokes;
+    // console.log(typeof jokes.result)
+    // const jokesItems = jokes.result.map((joke) =>
+    //   <li>{joke.value}</li>
+    // );
+    return (
+      <ul>jokesItems</ul>
+    );
+
+
+  }
 }
 
 export default class MainBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: "",
+      category: "",
+      jokes: null,
+    }
+    this.getJokes = this.getJokes.bind(this)
+  }
+
+  async fetchData(r) {
+    return await fetch('https://api.chucknorris.io/jokes/random')
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        return data
+      })
+  }
+
+  getJokes() {
+    if (this.state.selected === 'random') {
+      this.fetchData('random').then(joke => {
+        this.setState({jokes: joke}
+        )
+      })
+
+    }
+  }
+
+  toggleState = (e) => {
+    this.setState({selected: e.target.value})
+  }
 
   render() {
     return (
@@ -64,12 +130,31 @@ export default class MainBar extends Component {
           <p className="promo__content">Let’s try to find a joke for you:</p>
         </div>
         <div className="joke-search">
-        <CheckBox option="Random"/><br/>
-        <CheckBox option="From categories"/><br/>
-        <CheckBox option="Search"/>
-        {/*<Categories/>*/}
-        <button className="joke_search__button">Get a joke</button>
+          <label className="joke-search__label">
+            <input className="joke-search__option" value="random"
+                   checked={this.state.selected === 'random'}
+                   onChange={this.toggleState} type="radio"/>
+            Random
+          </label><br/>
+          <label className="joke-search__label">
+            <input className="joke-search__option" value="categories"
+                   checked={this.state.selected === 'categories'}
+                   onChange={this.toggleState} type="radio"/>
+            From Categories
+          </label><br/>
+          {this.state.selected === 'categories' ? <Categories/> : null}
+          <label className="joke-search__label">
+            <input className="joke-search__option" value="search"
+                   checked={this.state.selected === 'search'}
+                   onChange={this.toggleState} type="radio"/>
+            Search
+          </label><br/>
+          {this.state.selected === 'search' ? <input type="text"/> : null}
+          <button className="joke_search__button" onClick={this.getJokes}>Get a joke</button>
         </div>
+        <section className="jokes-section">
+          {this.state.jokes? <Jokes jokes={this.state.jokes}/> : null}
+        </section>
       </main>
     );
   }
